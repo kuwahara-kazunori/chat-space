81fd29ad420d8fc77e489c36a79d284266ef098e
kuwahara-kazunori <fightforliberty1993@gmail.com>
2020-07-16 00:33:54 +0900

    Create the HTML by receiving the returned JSON with the done method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html = 
+        `<div class="MessageBox">
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+        </div>
+      </div>`
+      return html;
+    } else {
+      let html = 
+      `<div class="MessageBox">
+        <div class="MessageInfo">
+          <div class="MessageInfo__userName">
+            ${message.user_name}
+          </div>
+          <div class="MessageInfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="Message">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
   $('.Form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +51,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
     })
   });
 });
