3779f7110da34b209b7adac63ae04c9f3862f026
kuwahara-kazunori <fightforliberty1993@gmail.com>
2020-07-18 20:15:16 +0900

    Fix

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -3,6 +3,25 @@ $(function(){
     if ( message.image ) {
       let html =
         `<div class="MessageBox" data-message-id=${message.id}>
+          <div class="MessageInfo">
+            <div class="MessageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="MessageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -15,27 +34,8 @@ $(function(){
           <p class="Message__content">
             ${message.content}
           </p>
-          <img class="Message__image" src="${message.image}">
         </div>
       </div>`
-      return html;
-    } else {
-      let html =
-      `<div class="MessageBox" data-message-id=${message.id}>
-      <div class="MessageInfo">
-        <div class="MessageInfo__userName">
-          ${message.user_name}
-        </div>
-        <div class="MessageInfo__date">
-          ${message.created_at}
-        </div>
-      </div>
-      <div class="Message">
-        <p class="Message__content">
-          ${message.content}
-        </p>
-      </div>
-    </div>`
       return html;
     };
   }
@@ -43,7 +43,7 @@ $(function(){
   $('.Form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
-    let url = $(this).attr('action')
+    let url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -54,13 +54,14 @@ $(function(){
     })
     .done(function(data){
       let html = buildHTML(data);
-      $('.MessageField').append(html);
-      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+      $('.MessageField').append(html);      
       $('form')[0].reset();
-      $('.Form__submit').prop('disabled', false);
+      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+      $('.Form__submit').prop("disabled", false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.Form__submit').prop("disabled", false);
     });
   });
 });
